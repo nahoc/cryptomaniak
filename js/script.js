8b40674ce6de138ddfbbfcd689b82337f78f225e
Cohan Carpentier <cohan@Cohans-MacBook-Pro.local>
2017-12-07 11:13:14 -0500

    es6 imports now work

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
+import {renderSettingsTab} from './renderTabs';
+
 const {
   ipcRenderer,
   shell
 } = require('electron');
+const con = require('electron').remote.getGlobal('console');
+
 
 // Navigation
 $('.tabButton').click(function () {
   $('.tabButton').removeClass('tabButtonActive');
-  $(this).addClass('tabButtonActive');
-
   $('.tab').hide();
 
   if (this.id === "marketTabButton") {
+    $(this).addClass('tabButtonActive');
     $('#marketTab').show();
     $('#balance').addClass('closed');
   }
   if (this.id === "portfolioTabButton") {
+    $(this).addClass('tabButtonActive');
     $('#portfolioTab').show();
     $('#balance').removeClass('closed');
+  }
+  if (this.id === "settingsTabButton") {
+    $('#settingsTab').show();
+    $('#balance').addClass('closed');
   }
 });
 
@@ -32,25 +40,45 @@ const formatPrice = (price) => {
 
 const renderMarketTab = (coinSymbol, coinName, coinPrice, coinPriceChange) => {
   document.querySelector('#marketTabCoins').innerHTML +=
-  '<li class="listItem">' +
-  '<img src="assets/coins/' + coinSymbol + '.svg" width="24" height="24" />' +
-  '<span class="coinName">' + coinName + '</span>' +
-  '<span class="coinPrice">$' + formatPrice(coinPrice) +
-  '<br/><span class="coinPriceChange ' + isNegative(coinPriceChange) + '">' + coinPriceChange + '%</span></span>' +
-  '</li>';
+    '<li class="listItem">' +
+    '<img src="assets/coins/' + coinSymbol + '.svg" width="24" height="24" />' +
+    '<span class="coinName">' + coinName + '</span>' +
+    '<span class="coinPrice">$' + formatPrice(coinPrice) +
+    '<br/><span class="coinPriceChange ' + isNegative(coinPriceChange) + '">' + coinPriceChange + '%</span></span>' +
+    '</li>';
 }
 
 const renderPortfolioTab = (coinSymbol, coinName) => {
   document.querySelector('#portfolioTabCoins').innerHTML +=
-  '<li class="listItem">' +
-  '<img src="assets/coins/' + coinSymbol + '.svg" width="24" height="24" />' +
-  '<span class="coinName">' + coinName + '</span>' +
-  '<input type="text" class="input coinQuantity" placeholder="0">' +
-  '</li>';
+    '<li class="listItem">' +
+    '<img src="assets/coins/' + coinSymbol + '.svg" width="24" height="24" />' +
+    '<span class="coinName">' + coinName + '</span>' +
+    '<input type="text" class="input coinQuantity" placeholder="0">' +
+    '</li>';
 }
 
-const updateView = (marketTabCoins) => {
-  marketTabCoins.forEach(element => {
+/*const renderSettingsTab = (exchangeRatesObject) => {
+  let exchangeRatesArray;
+  let exchangeRatesSelect = '<div class="select"><select>';
+  
+  exchangeRatesArray = $.map(exchangeRatesObject, function (value, index) {
+    return [index];
+  });
+
+  exchangeRatesArray.forEach(element => {
+    exchangeRatesSelect += '<option>' + element + '</option>';
+  });
+
+  exchangeRatesSelect += '</select></div>';
+
+  document.querySelector('#settingsTab').innerHTML +=
+  '<li class="listItem">' +
+    '<span class="settingName">Currency</span>' + exchangeRatesSelect;
+  '</li>';
+}*/
+
+const updateTabView = (coins) => {
+  coins.forEach(element => {
     const coinName = element.name;
     const coinPrice = element.price_usd;
     const coinSymbol = element.symbol;
@@ -60,6 +88,11 @@ const updateView = (marketTabCoins) => {
   });
 }
 
+const updateSettingsTabView = (exchangeRates) => {
+  const exchangeRatesObject = exchangeRates.rates;
+  renderSettingsTab(exchangeRatesObject);
+}
+
 const getMarketTabCoins = () => {
   const url = 'https://api.coinmarketcap.com/v1/ticker/?limit=9';
 
@@ -67,7 +100,7 @@ const getMarketTabCoins = () => {
     .then(
       function (response) {
         response.json().then(function (data) {
-          updateView(data);
+          updateTabView(data);
         });
       }
     )
@@ -76,5 +109,22 @@ const getMarketTabCoins = () => {
     });
 }
 
+const getExchangeRates = () => {
+  const url = 'https://api.fixer.io/latest?base=USD';
+
+  fetch(url)
+    .then(
+      function (response) {
+        response.json().then(function (data) {
+          updateSettingsTabView(data);
+        });
+      }
+    ).catch(function (err) {
+      // error
+    });
+}
+
 // Update initial market coins when loaded
 document.addEventListener('DOMContentLoaded', getMarketTabCoins);
+// Update settings when currencies loaded
+document.addEventListener('DOMContentLoaded', getExchangeRates);
